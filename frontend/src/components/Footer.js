1cd7c336afa1033964433e9d4721bdceaa27dffe
Aleksa Milosavljevic <aleksa.milosavljevic1998@gmail.com>
2022-06-27 13:38:11 +0200

    Sitna izmena

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className="text-center py-3">
-                         &copy; Aleksa & Nemanja
+                         &copy; Aleksa & Nemanja & Nikolina
                     </Col>
                 </Row>
             </Container>
